6054dad134aa284a47b1ab955cd17eef04de004f
Robson Martins <robsonsmartins@gmail.com>
2022-05-07 22:33:49 -0300

    Rev.E. Updated cmake scripts. Updated documentation.

--- firmware/usbflashprog/scripts/test.sh
+++ firmware/usbflashprog/scripts/test.sh
@@ -5,13 +5,13 @@ echo "Generating build files..."
 rm -Rf ${workspaceRoot}/build
 cmake -B ${workspaceRoot}/build -S ${workspaceRoot} -DTEST_BUILD=ON
 echo "Building test project..."
-pushd ${workspaceRoot}/build
-make
-popd
+cmake --build ${workspaceRoot}/build
 echo "Running tests..."
-${workspaceRoot}/build/test/usbflashprog_test
+pushd ${workspaceRoot}/build
+ctest
+popd
 echo "Capturing coverage info..."
-lcov --directory ${workspaceRoot}/build/ --capture --output-file ${workspaceRoot}/build/coverage.info -rc lcov_branch_coverage=1 --exclude \/usr\/include\/\* --exclude \/usr\/local\/include\/\* --exclude ${workspaceRoot}/test\/\*
+lcov --directory ${workspaceRoot}/build/ --capture --output-file ${workspaceRoot}/build/coverage.info -rc lcov_branch_coverage=1 --exclude \/usr\/include\/\* --exclude \/usr\/local\/include\/\* --exclude ${workspaceRoot}\/build\/\* --exclude ${workspaceRoot}\/test\/\*
 echo "Generating coverage report..."
 genhtml ${workspaceRoot}/build/coverage.info --output-directory ${workspaceRoot}/../../docs/firmware/lcov/
 echo "Success!"

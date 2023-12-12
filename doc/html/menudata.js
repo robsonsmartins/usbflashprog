b207bd7c2eca835a24b4fae05ee719dcb7547769
Robson Martins <robsonsmartins@gmail.com>
2023-12-12 19:16:16 -0300

    simulator: add the code documentation

---
+++ doc/html/menudata.js
@@ -0,0 +1,81 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var menudata={children:[
+{text:"Main Page",url:"index.htm"},
+{text:"Related Pages",url:"pages.htm"},
+{text:"Modules",url:"modules.htm"},
+{text:"Classes",url:"annotated.htm",children:[
+{text:"Class List",url:"annotated.htm"},
+{text:"Class Hierarchy",url:"hierarchy.htm"},
+{text:"Class Members",url:"functions.htm",children:[
+{text:"All",url:"functions.htm",children:[
+{text:"b",url:"functions.htm#index_b"},
+{text:"c",url:"functions.htm#index_c"},
+{text:"e",url:"functions.htm#index_e"},
+{text:"f",url:"functions.htm#index_f"},
+{text:"g",url:"functions.htm#index_g"},
+{text:"m",url:"functions.htm#index_m"},
+{text:"o",url:"functions.htm#index_o"},
+{text:"r",url:"functions.htm#index_r"},
+{text:"s",url:"functions.htm#index_s"},
+{text:"w",url:"functions.htm#index_w"},
+{text:"~",url:"functions.htm#index__7E"}]},
+{text:"Functions",url:"functions_func.htm",children:[
+{text:"b",url:"functions_func.htm#index_b"},
+{text:"c",url:"functions_func.htm#index_c"},
+{text:"e",url:"functions_func.htm#index_e"},
+{text:"f",url:"functions_func.htm#index_f"},
+{text:"g",url:"functions_func.htm#index_g"},
+{text:"m",url:"functions_func.htm#index_m"},
+{text:"o",url:"functions_func.htm#index_o"},
+{text:"r",url:"functions_func.htm#index_r"},
+{text:"s",url:"functions_func.htm#index_s"},
+{text:"w",url:"functions_func.htm#index_w"},
+{text:"~",url:"functions_func.htm#index__7E"}]}]}]},
+{text:"Files",url:"files.htm",children:[
+{text:"File List",url:"files.htm"},
+{text:"File Members",url:"globals.htm",children:[
+{text:"All",url:"globals.htm",children:[
+{text:"c",url:"globals.htm#index_c"},
+{text:"d",url:"globals.htm#index_d"},
+{text:"g",url:"globals.htm#index_g"},
+{text:"i",url:"globals.htm#index_i"},
+{text:"l",url:"globals.htm#index_l"},
+{text:"m",url:"globals.htm#index_m"},
+{text:"n",url:"globals.htm#index_n"},
+{text:"p",url:"globals.htm#index_p"},
+{text:"r",url:"globals.htm#index_r"},
+{text:"s",url:"globals.htm#index_s"}]},
+{text:"Functions",url:"globals_func.htm",children:[
+{text:"c",url:"globals_func.htm#index_c"},
+{text:"d",url:"globals_func.htm#index_d"},
+{text:"g",url:"globals_func.htm#index_g"},
+{text:"i",url:"globals_func.htm#index_i"},
+{text:"l",url:"globals_func.htm#index_l"},
+{text:"m",url:"globals_func.htm#index_m"},
+{text:"n",url:"globals_func.htm#index_n"},
+{text:"p",url:"globals_func.htm#index_p"},
+{text:"r",url:"globals_func.htm#index_r"},
+{text:"s",url:"globals_func.htm#index_s"}]}]}]}]}
